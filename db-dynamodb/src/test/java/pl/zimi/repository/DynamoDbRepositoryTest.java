35c53f2c5397e8757febdfc2ba4ccb91b3341989
zimi <reiter.szymon@gmail.com>
2024-11-24 20:01:22 +0100

    LocalStack

--- db-dynamodb/src/test/java/pl/zimi/repository/DynamoDbRepositoryTest.java
+++ db-dynamodb/src/test/java/pl/zimi/repository/DynamoDbRepositoryTest.java
@@ -1,8 +1,8 @@
 package pl.zimi.repository;
 
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
 import pl.zimi.repository.contract.Contract;
 import pl.zimi.repository.contract.ContractVerificator;
 import pl.zimi.repository.example.Foo;
@@ -10,8 +10,12 @@ import pl.zimi.repository.example.SFoo;
 import pl.zimi.repository.query.Queries;
 import pl.zimi.repository.query.Query;
 import pl.zimi.repository.query.Repository;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
 import java.util.List;
 import java.util.function.Function;
@@ -21,12 +25,62 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class DynamoDbRepositoryTest {
 
+    private static LocalStackContainer localstack;
+
+    private static DynamoDbClient client;
+
+    @BeforeAll
+    public static void init() {
+        localstack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.11.3"))
+                .withServices(LocalStackContainer.Service.DYNAMODB);
+        localstack.start();
+
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.builder()
+                        .accessKeyId(localstack.getAccessKey())
+                        .secretAccessKey(localstack.getSecretKey()).build()
+        );
+
+        client = DynamoDbClient.builder()
+                .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.DYNAMODB))
+                .region(Region.of(localstack.getRegion()))
+                .credentialsProvider(credentialsProvider)
+                .build();
+
+        CreateTableRequest request = CreateTableRequest.builder()
+                .tableName("flashcards-dev-Foo")
+                .attributeDefinitions(
+                        AttributeDefinition.builder()
+                                .attributeName("id")
+                                .attributeType(ScalarAttributeType.S)
+                                .build()
+                )
+                .keySchema(
+                        KeySchemaElement.builder()
+                                .attributeName("id")
+                                .keyType(KeyType.HASH)
+                                .build()
+                )
+                .provisionedThroughput(
+                        ProvisionedThroughput.builder()
+                                .readCapacityUnits(10L)
+                                .writeCapacityUnits(5L)
+                                .build()
+
+                )
+                .build();
+
+        client.createTable(request);
+    }
+    @AfterAll
+    public static void close() {
+        localstack.close();
+    }
+
     @TestFactory
     List<DynamicTest> repositoryTest() {
         final Contract<Foo> contract = Contract.repository(Foo.class).id(SFoo.foo.id).version(SFoo.foo.version);
-        DynamoDbClient client = DynamoDbClient.builder()
-                .region(Region.EU_CENTRAL_1)
-                .build();
+
 
         String prefix = "flashcards-dev-";
         Function<Contract<Foo>, Repository<Foo>> port = c -> {
